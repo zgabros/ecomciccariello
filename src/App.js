4cd433f64f21d7f0e5b89cb73b37f31df2a49f25
Gabriel <zgabros@gmail.com>
2022-05-24 13:02:18 -0300

    navBar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className='App'>
+      <NavBar></NavBar>
       <div className='App-header'>
       <h1>eComerce</h1>
       <p>Camada 31205. Gabriel Ciccariello</p>
